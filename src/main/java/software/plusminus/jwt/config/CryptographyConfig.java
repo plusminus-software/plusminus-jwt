0834faf3d1d8df6b108d6d01a5ea05d15f2532b4
Taras Shpek <taras.shpek@gmail.com>
2021-08-10 22:30:20 +0300

    support properties along with env variables

--- src/main/java/software/plusminus/jwt/config/CryptographyConfig.java
+++ src/main/java/software/plusminus/jwt/config/CryptographyConfig.java
@@ -1,10 +1,11 @@
 package software.plusminus.jwt.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -14,42 +15,40 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
+import static com.fasterxml.jackson.core.JsonEncoding.UTF8;
+
 @Configuration
 public class CryptographyConfig {
 
-    @Value("#{environment.JWT_PRIVATE_KEY}")
-    private String privateKeyPem;
-
-    @Value("#{environment.JWT_PUBLIC_KEY}")
-    private String publicKeyPem;
-
     @Bean
-    public PrivateKey privateKey() throws IOException,
+    public PrivateKey privateKey(JwtProperties jwtProperties) throws IOException,
             NoSuchAlgorithmException, InvalidKeySpecException {
 
-        String key = privateKeyPem.replaceAll("\\n", "")
-                .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replace("-----END PRIVATE KEY-----", "")
-                .replaceAll(" ", "")
-                .trim();
-        PKCS8EncodedKeySpec keySpecPkcs8 = new PKCS8EncodedKeySpec(
-                Base64.getDecoder().decode(key));
-        return keyFactory().generatePrivate(keySpecPkcs8);
+        try (InputStream is = jwtProperties.getPrivateKey().getInputStream()) {
+            String key = IOUtils.toString(is, UTF8.getJavaName())
+                    .replaceAll("\\n", "")
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "");
+            PKCS8EncodedKeySpec keySpecPkcs8 = new PKCS8EncodedKeySpec(
+                    Base64.getDecoder().decode(key));
+            return keyFactory().generatePrivate(keySpecPkcs8);
+        }
     }
 
     @Bean
-    public RSAPublicKey publicKey() throws IOException,
+    public RSAPublicKey publicKey(JwtProperties jwtProperties) throws IOException,
             NoSuchAlgorithmException, InvalidKeySpecException {
 
-        String key = publicKeyPem.replaceAll("\\n", "")
-                .replace("-----BEGIN PUBLIC KEY-----", "")
-                .replace("-----END PUBLIC KEY-----", "")
-                .replaceAll(" ", "")
-                .trim();
+        try (InputStream is = jwtProperties.getPublicKey().getInputStream()) {
+            String key = IOUtils.toString(is, UTF8.getJavaName())
+                    .replaceAll("\\n", "")
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "");
 
-        X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(
-                Base64.getDecoder().decode(key));
-        return (RSAPublicKey) keyFactory().generatePublic(keySpecX509);
+            X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(
+                    Base64.getDecoder().decode(key));
+            return (RSAPublicKey) keyFactory().generatePublic(keySpecX509);
+        }
     }
 
     @Bean
