c103af681ea37f5ce5631791a2ef9ab822121555
Taras Shpek <taras.shpek@gmail.com>
2021-10-31 03:15:34 +0300

    add autoconfig class

---
+++ src/main/java/software/plusminus/jwt/config/JwtAutoconfig.java
@@ -0,0 +1,10 @@
+package software.plusminus.jwt.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("software.plusminus.jwt")
+public class JwtAutoconfig {
+}
+
